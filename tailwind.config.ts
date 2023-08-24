b3cd6a73810241d13e7fd099643a08861708630d
Kevin Osorio <kevinosorio.code@gmail.com>
2023-08-24 15:18:47 -0400

    update app styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/infrastructure/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
