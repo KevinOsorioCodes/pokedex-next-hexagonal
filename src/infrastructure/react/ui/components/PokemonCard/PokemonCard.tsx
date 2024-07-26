77bce5e2fced91a29a92e97d4347ce4d2579f58e
kevinosorio <kevinosorio.code@gmail.com>
2024-07-26 16:33:47 -0400

    add pokemon types color

--- src/infrastructure/react/ui/components/PokemonCard/PokemonCard.tsx
+++ src/infrastructure/react/ui/components/PokemonCard/PokemonCard.tsx
@@ -1,6 +1,28 @@
 import { usePokemonDetailsContext } from '~/infrastructure/react/hooks/usePokemonDetailsContext'
 import Image from 'next/image'
 
+const typesColors = {
+  normal: 'bg-gray-500',
+  fire: 'bg-red-500',
+  water: 'bg-blue-500',
+  electric: 'bg-yellow-500',
+  grass: 'bg-green-500',
+  ice: 'bg-teal-500',
+  fighting: 'bg-orange-500',
+  poison: 'bg-purple-500',
+  ground: 'bg-yellow-700',
+  flying: 'bg-indigo-500',
+  psychic: 'bg-pink-500',
+  bug: 'bg-lime-500',
+  rock: 'bg-yellow-600',
+  ghost: 'bg-indigo-700',
+  dragon: 'bg-purple-700',
+  dark: 'bg-gray-700',
+  steel: 'bg-gray-400',
+  fairy: 'bg-pink-300',
+}
+export const useColorType = () => {}
+
 export const PokemonCard = () => {
   const { pokemon } = usePokemonDetailsContext()
 
--- src/infrastructure/react/ui/components/PokemonList/index.tsx
+++ src/infrastructure/react/ui/components/PokemonList/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { labels } from '~/shared/labels'
 import { usePokemonDetailsContext } from '~/infrastructure/react/hooks/usePokemonDetailsContext'
 import { PokemonDTO } from '~/infrastructure/services/dtos/PokemonPaginated.dto'
@@ -9,21 +9,41 @@ interface IPokemonListProps {
 
 const PokemonList: FC<IPokemonListProps> = ({ pokemons }) => {
   const { handleSelectPokemon } = usePokemonDetailsContext()
+
+  useEffect(() => {
+    if (pokemons?.length) {
+      handleSelectPokemon(pokemons[0].name)
+    }
+  }, [])
   return (
     <section
       className={
-        'flex flex-col w-1/4 items-center gap-2 h-5/6 overflow-y-scroll '
+        'flex flex-col w-1/4 items-center gap-4 h-full overflow-y-scroll '
       }
     >
-      <h5 className={'font-bold text-xl'}>{labels.POKEDEX}</h5>
+      <h5
+        className={
+          'font-bold text-xl fixed bg-black w-60 flex flex-col items-center justify-center h-10'
+        }
+      >
+        {labels.POKEDEX}
+      </h5>
       {!pokemons && <div>loading...</div>}
       {pokemons && (
-        <ol data-testid={'pokedex-list'}>
+        <ol
+          data-testid={'pokedex-list'}
+          className={
+            'w-4/6 gap-2.5 flex flex-col items-center justify-center mt-10'
+          }
+        >
           {pokemons.map((pokemon, index) => (
             <li
               onClick={() => handleSelectPokemon(pokemon.name)}
               data-testid={`pokemon-${pokemon.name}`}
               key={`${pokemon.name}-${index}`}
+              className={
+                'cursor-pointer  w-full p-4 p border-2 flex flex-col items-center justify-center border-red-600'
+              }
             >
               {pokemon.name}
             </li>
